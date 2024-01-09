34b6bf763b48b9df491a443750b52ebe1027e8f2
Francisco Gray <francisco.gray@instructure.com>
2024-01-09 09:27:00 -0800

    Adds AFT Sandbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "francisco.gray+salumi-aft-sandbox-sft@instructure.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "francisco.gray@instructure.com"
+    SSOUserFirstName          = "Francisco"
+    SSOUserLastName           = "Gray"
   }
 
   account_tags = {
